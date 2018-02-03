1a4468ce861ce83d66fa8b1fc709d8abacbeef1d
Krzysztof Gawryś <kgawrys3@gmail.com>
2018-02-03 18:52:59 +0100

    First migration - ticker table

---
+++ src/main/resources/db/migrations/V1__initial.sql
@@ -0,0 +1,18 @@
+CREATE TABLE tickers (
+    id serial PRIMARY KEY,
+    name TEXT NOT NULL,
+    symbol TEXT NOT NULL,
+    rank integer NOT NULL,
+    price_usd numeric(20,8) NOT NULL,
+    price_btc numeric(20,8) NOT NULL,
+    volume_usd_24h numeric(20,2) NOT NULL,
+    market_cap_usd numeric(20,2) NOT NULL,
+    available_supply numeric(20,2) NOT NULL,
+    total_supply numeric(20,2) NOT NULL,
+    max_supply numeric(20,2) NOT NULL,
+    percent_change_1h numeric(3,2) NOT NULL,
+    percent_change_24h numeric(3,2) NOT NULL,
+    percent_change_7d numeric(3,2) NOT NULL,
+    last_updated timestamp with time zone NOT NULL
+);
+
