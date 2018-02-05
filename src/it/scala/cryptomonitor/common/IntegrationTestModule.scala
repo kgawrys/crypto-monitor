5781a9f9c9bd85f4a000509787af33c3e1418d5e
Krzysztof Gawryś <kgawrys3@gmail.com>
2018-02-05 18:49:41 +0100

    Fixed test database configuration in it:tests

--- src/it/scala/cryptomonitor/common/IntegrationTestModule.scala
+++ src/it/scala/cryptomonitor/common/IntegrationTestModule.scala
@@ -4,14 +4,19 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.testkit.{RouteTestTimeout, ScalatestRouteTest}
 import akka.testkit._
+import com.typesafe.config.ConfigFactory
 import cryptomonitor.core.Setup
 import cryptomonitor.core.json.CryptoMonitorJsonSupport
+import slick.jdbc.PostgresProfile.api._
 import org.scalatest._
 
 import scala.concurrent.duration._
 
 trait IntegrationTestModule extends Setup with CryptoMonitorJsonSupport with ScalatestRouteTest with BeforeAndAfterAll with Matchers {
   self: Suite =>
+
+  override lazy val config = ConfigFactory.load()
+  override lazy val db     = Database.forConfig("postgres", config)
 
   implicit def default(implicit system: ActorSystem) = RouteTestTimeout(1.minute dilated system)
 
